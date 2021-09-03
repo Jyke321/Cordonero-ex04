eb28028fcd352c386fe61e8da1c61839ccf15b54
Jyke321 <jyke321@gmail.com>
2021-09-03 14:19:15 -0400

    Update 1

--- src/main/java/exercise04/Solution04.java
+++ src/main/java/exercise04/Solution04.java
@@ -1,12 +1,19 @@
 package exercise04;
 
+/*
+*  UCF COP3330 Fall 2021 Assignment 1 Solution
+*  Copyright 2021 Jacob Cordonero
+ */
+
 import java.util.Scanner;
 
 public class Solution04 {
     public static void main(String[] args) {
         //prompts user for a noun, a verb,
         //an adverb, and an adjective
-        //and prints them into a story
+        //and stores them into strings
+        //then prints them into a story
+        //using concatenation
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a noun: ");
         String noun = input.nextLine();
